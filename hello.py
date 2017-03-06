de47034c74fed6d77d1ba53a00351a419531e667
Al_ <junkmail954@yahoo.com>
2017-03-06 21:56:35 +0000

    End of Lesson 28

--- hello.py
+++ hello.py
@@ -1,13 +1,26 @@
 import os
 
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return url_for('show_user_profile', username='richard')
+    
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for passed user
+    return "User %s" % username
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return "Post {}".format(post_id)
+
 
 if __name__ == '__main__':
     host = os.getenv('IP', '0.0.0.0')   # for cloud 9
     port = int(os.getenv('PORT',5000))
+    app.debug = True
     app.run(host=host, port=port)
