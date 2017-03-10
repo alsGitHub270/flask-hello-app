3af6c9071aac68846ae3d7fe7b465d4880850924
Al_ <junkmail954@yahoo.com>
2017-03-10 20:37:38 +0000

    Added Sessions, Logging and Secret Key

---
+++ hello.6.py
@@ -0,0 +1,47 @@
+import os
+
+from flask import Flask, request, render_template, redirect, url_for, flash, make_response
+app = Flask(__name__)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    error = None
+    if request.method == "POST":
+        if valid_login(request.form['username'], request.form['password']):
+            flash("Successfully logged in")
+            response = make_response(redirect(url_for('welcome')))
+            response.set_cookie('username', request.form.get('username'))
+            return response
+        else:
+            error = "Incorrect username and password"
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    response = make_response(redirect(url_for('login')))
+    response.set_cookie('username', '', expires=0)
+    return response
+
+
+@app.route('/')
+def welcome():
+    username = request.cookies.get('username')
+    if username:
+        return render_template('welcome.html', username=username)
+    else:
+        return redirect(url_for('login'))
+    
+
+def valid_login(username, password):
+    if username == password:
+        return True
+    return False
+    
+
+if __name__ == '__main__':
+    host = os.getenv('IP', '0.0.0.0')   # for cloud 9
+    port = int(os.getenv('PORT',5000))
+    app.debug = True
+    app.secret_key = 'SuperSecretKey'
+    app.run(host=host, port=port)
